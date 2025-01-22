73ec8a19db55ea8516f6c6bcd886fb9583be3f8f
hmsou <hmarcs66@gmail.com>
2025-01-22 13:49:58 -0300

    Add Player entity

--- src/main/java/com/hmso/premier_league/domain/Player.java
+++ src/main/java/com/hmso/premier_league/domain/Player.java
@@ -1,4 +1,36 @@
 package com.hmso.premier_league.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "players")
 public class Player {
+    @Id
+    @Column(name = "name", unique = true)
+    private String name;
+    private String nation;
+    private String pos;
+    private Integer age;
+    private Integer mp;
+    private Integer stars;
+    private Double min;
+    private Double gls;
+    private Double ast;
+    private Double pk;
+    private Double crdy;
+    private Double crdr;
+    private Double xg;
+    private Double xag;
+    private String team;
 }
