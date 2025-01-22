3f2f4190f76d0b8926a4eef6010a131c7ed1cc25
hmsou <hmarcs66@gmail.com>
2025-01-22 13:52:13 -0300

    Add methods for Player repository

--- src/main/java/com/hmso/premier_league/repository/PlayerRepository.java
+++ src/main/java/com/hmso/premier_league/repository/PlayerRepository.java
@@ -3,5 +3,9 @@ package com.hmso.premier_league.repository;
 import com.hmso.premier_league.domain.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PlayerRepository extends JpaRepository<Player, String> {
+    void deleteByName(String playerName);
+    Optional<Player> findByName(String name);
 }
